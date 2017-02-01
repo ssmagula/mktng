70626c5dd383a913aa21049b85041c976050d4ea
ssmagula <stefan.smagul@gmail.com>
2017-01-31 18:11:18 -0600

    Optimizes hero heading for small screen, improves email from info

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,9 +17,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'stefan.smagula@gmail.com'; //  SMAG: This is where the form will send a message.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "HydrogeNXT Contact Form:  $name";
+$email_body = "You have received a new message from the HydrogeNXT contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yhydrogenxt.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
